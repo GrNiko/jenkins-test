20deb9764f538d6fd13922838eee0e24479b8cd4
grNiko <grNIko91@gmail.com>
2022-02-14 19:13:16 +0700

    clean

--- src/main/java/Butler.java
+++
@@ -1,5 +0,0 @@
-public class Butler {
-    public static void main(String[] args) {
-        System.out.println("Hello ver.3");
-    }
-}
--- src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h5>Hello ver.1</h5>
-</body>
-</html>
